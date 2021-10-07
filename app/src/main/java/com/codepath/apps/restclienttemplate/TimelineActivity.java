713623f28b0d91638b22bbe354c4cd4f4b9111a7
kp <kp55@njit.edu>
2021-10-07 01:26:56 -0400

    sync fix

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -91,7 +91,7 @@ public class TimelineActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-                Log.e(TAG, "onFailure for loadMoreData ", throwable);
+                Log.e(TAG, "onFailure ", throwable);
             }
         }, tweets.get(tweets.size()-1).id);
     }
